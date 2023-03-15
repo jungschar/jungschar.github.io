3b067db799f65ea4d43a50d3544cc80010efa41b
Elias Theis <eliastheis.sg@gmail.com>
2023-03-15 13:09:18 +0100

    Fixed copy/paste error

--- docs/js/search.js
+++ docs/js/search.js
@@ -1,12 +1,12 @@
 const searchForm = document.getElementById('search-form');
 
-myForm.addEventListener('submit', function(event) {
+searchForm.addEventListener('submit', function(event) {
 
     // prevent the form from submitting
     event.preventDefault(); 
 
     // get the form data
-    const formData = new FormData(myForm);
+    const formData = new FormData(searchForm);
     const params = {};
 
     // iterate over the form data to build the params object
